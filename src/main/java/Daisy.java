b49cc8d2590bb5bc2c7e245d7e5ae6994c0cd762
RozalieSmit <>
2025-02-06 14:54:46 +0800

    commit add and list function

--- src/main/java/Daisy.java
+++ src/main/java/Daisy.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Daisy {
+    private static final int MAX_ITEMS = 100;
+    private static String[] items = new String[MAX_ITEMS];
+    private static int itemCount = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,12 +24,38 @@ public class Daisy {
             input = scanner.nextLine();
             if (input.equals("bye")){
                 break;
+            } else if (input.equals("list")){
+                listItems();
+            } else {
+                addItem(input);
             }
             System.out.println(input);
         }
 
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+    }
+
+    private static void listItems() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < itemCount; i++) {
+            System.out.println((i + 1) + ". " + items[i]);
+        }
+    }
+
+    private static void addItem(String item) {
+        if (itemCount < MAX_ITEMS) {
+            items[itemCount] = item;
+            itemCount++;
+            System.out.println("____________________________________________________________");
+            System.out.println(" added: " + item);
+            System.out.println("____________________________________________________________");
+        }
+        else {
+            System.out.println("____________________________________________________________");
+            System.out.println("The list is full. Cannot add any more items.");
+            System.out.println("____________________________________________________________");
+        }
     }
 }
 
